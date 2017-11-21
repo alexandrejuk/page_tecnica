2b1a2c778c681cac5c7b7f398ec8df56b09b2d03
alexandrejuk <ale_santos.soares@hotmail.com>
2017-11-21 17:56:29 -0200

    modify somethings functions and methods

--- src/app/tecnica/tecnica.component.ts
+++ src/app/tecnica/tecnica.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs/Rx';
+
+import { DragulaService } from 'ng2-dragula/components/dragula.provider';
+
 import { TecnicaService } from './tecnica.service';
 import { TECNICOS } from './tecnicos.mock';
 
@@ -12,10 +15,22 @@ export class TecnicaComponent implements OnInit {
 
   public atendimentos$: Observable<any[]>;
   public tecnicos = TECNICOS;
-  constructor(private tecnicaService: TecnicaService) { }
+  constructor(private tecnicaService: TecnicaService,
+              private dragulaService: DragulaService) {
+
+                dragulaService.drag.subscribe((value) => {
+                  this.onDrag(value.slice(1));
+                });
+              }
 
   ngOnInit() {
     this.atendimentos$ = this.tecnicaService.getAtendimentos();
   }
 
+
+  private onDrag(args) {
+    const [e, el] = args;
+    console.log(e, el);
+    console.log(args);
+  }
 }
