17fec4932a27ed77e1c991d0e0ed255197385fea
alexandrejuk <ale_santos.soares@hotmail.com>
2017-11-21 16:10:04 -0200

    modify tecnicos mock and db.json

--- src/app/tecnica/tecnicos.mock.ts
+++ src/app/tecnica/tecnicos.mock.ts
@@ -1,1 +1,5 @@
-export const TECNICOS = ['Rafael Neves', 'Vitor Silva Lima', 'Alexandre Dos Santos', 'Emerson'];
+export const TECNICOS = [
+  { id: 1, nome: 'Rafael Neves', atendimentos: [] },
+  { id: 2, nome: 'Felipe Franfresco', atendimentos: [] },
+  { id: 3, nome: 'Alexandre dos Santos Lindo', atendimentos: [] },
+  { id: 4, nome: 'Vitor Avenida dos Travecos', atendimentos: [] }];
