090c090a9603f209d97bdeec82aef6d0805804f2
alexandrejuk <ale_santos.soares@hotmail.com>
2017-11-22 15:12:04 -0200

    create a component of drag and drop
    created drag and drop custom to modify the datas of object
    atendimento.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
 
 import { DragulaModule } from 'ng2-dragula';
 import { AppComponent } from './app.component';
 import { TecnicaComponent } from './tecnica/tecnica.component';
 import { TecnicaService } from './tecnica/tecnica.service';
-
+import { DndModule } from 'ng2-dnd';
 @NgModule({
   declarations: [
     AppComponent,
@@ -15,6 +16,8 @@ import { TecnicaService } from './tecnica/tecnica.service';
   imports: [
     BrowserModule,
     DragulaModule,
+    DndModule.forRoot(),
+    FormsModule,
     HttpModule
   ],
   providers: [TecnicaService],
